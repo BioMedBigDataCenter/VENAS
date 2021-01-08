3a14f80964caeecccc33609d42457c3d20c7f318
qianjiaqiang <75718448+qianjiaqiang@users.noreply.github.com>
2021-01-08 10:25:25 +0800

    Update main_path_example.py

--- main_path_example.py
+++ main_path_example.py
@@ -1,11 +1,10 @@
 import main_path as main_path
+def run():
+    table=main_path.impcsv("net.csv")
+    G=main_path.generateGraph(table)
+    coms=main_path.getCluster(G)
+    return_path,filterNode,KnodeList = main_path.process(G,coms)
+    main_path.createGephiNodeCSV(coms,return_path,KnodeList,filterNode,G)
 
-table=main_path.impcsv("net.csv")
-
-G=main_path.generateGraph(table)
-
-coms=main_path.getCluster(G)
-
-return_path,filterNode,KnodeList = main_path.process(G,coms)
-
-main_path.createGephiNodeCSV(coms,return_path,KnodeList,filterNode,G)
+if __name__ == "__main__":
+    run()
