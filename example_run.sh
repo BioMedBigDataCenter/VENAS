8958ceffb1987f72fc9c9d981a5064399399ef4a
Alexander Chernoskutov <alecherov@gmail.com>
2021-05-08 10:55:54 +0500

    Add example_run.sh

---
+++ example_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+PY=python3
+
+# Part 1
+echo
+echo "Part 1"
+echo
+$PY -u parsimony-informative.py -i example_data -m variation_graph_taxonid_2697049_outgroupid_none.ma -b none -r 0 -f "OEAV139851"
+
+# Part 2
+echo
+echo "Part 2"
+echo
+$PY -u haplotype_network.py example_data
+
+# Filter net_all.txt
+awk -F'\t' '{print $1","$2}' example_data/net_all.txt > net.csv
+sed -i '1i\Source,Target' net.csv
+
+# Part 3
+echo
+echo "Part 3"
+echo
+$PY main_path_example.py
