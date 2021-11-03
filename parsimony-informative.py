698a88e775501663db1ce2cb21e539eebdc376d5
qianjiaqiang <75718448+qianjiaqiang@users.noreply.github.com>
2021-11-03 11:10:50 +0800

    Update parsimony-informative.py

--- parsimony-informative.py
+++ parsimony-informative.py
@@ -3,15 +3,6 @@
 # __author__  = Ruifang Cao
 # __python__  = 3.+
 
-"""
-查找alignment结果中的简并信息位点位点所在位置，
-这个位点至少有两种以上碱基的情况，同时不同碱基的出现评率大于2
-v1.6:将remove非“ATGC”的碱基的基因组去除脚本和简约信息位点脚本合并为一个脚本，只查找一遍PIS，
-先根据PIS筛选掉一些基因组，然后计算这些PIS对应的评率
-这一版本的文件会比pis_remove_N.py剔除的基因组更多，因为数据pis_remove_N.py脚本仅剔除对应频率在是N或者-的碱基
-而这个脚本简并碱基，例如Y，S的基因组也剔除
-"""
-
 from Bio import AlignIO, SeqIO
 import os
 from tqdm import tqdm
@@ -19,8 +10,6 @@ import numpy as np
 import argparse
 import sys
 
-
-# 第一轮查找，all_pis为所有有效简约信息文件，rm_pis是根据用户给的阈值筛选一定频率以上用作晒酸基因组在该pis位点均是ATGC碱基的pis位点
 def find_ps_sites(alignment, seq, remove_fq, epis_base):
     rm_pis = []
     all_pis = []
@@ -38,7 +27,7 @@ def find_ps_sites(alignment, seq, remove_fq, epis_base):
             else:
                 uniq_nc[tmp_key] = 1
                 tmp_all += 1
-        # 有效碱基数大于base
+        # The number of effective bases is greater than the parameter "base"
         if len(uniq_nc) > 1 and tmp_all > epis_base:
             cur = 0
             for key in uniq_nc.keys():
@@ -49,7 +38,7 @@ def find_ps_sites(alignment, seq, remove_fq, epis_base):
                 max_dic = max(uniq_nc, key=uniq_nc.get)
                 pos_freq = 1 - (uniq_nc[max_dic] / tmp_all)
                 all_pis.append(width)
-                # remove_fq 是用户定义的需要删除那些频率以内的碱基是非 “ATCG”的情况
+                # remove_fq is a user-defined parameter for which frequencies need to be removed
                 if pos_freq > remove_fq:
                     rm_pis.append(width)
     return all_pis, rm_pis
@@ -62,7 +51,6 @@ def pis_freq(align_filter, sites, seq, f_base):
         uniq_nc = {}
         tmp_all = 0
         tmp_freq = []
-        # fasta文件前n条序列是outgroup序列，从outgroup序列后统计简约位点
         for j in range(0, len(align_filter)):
             tmp_key = align_filter[j].seq[width]
             if tmp_key not in default_nc:
@@ -73,8 +61,7 @@ def pis_freq(align_filter, sites, seq, f_base):
             else:
                 uniq_nc[tmp_key] = 1
                 tmp_all += 1
-        # 有效碱基数大于1000 and tmp_all > 1000
-        # rm_non-ATCG_genomes.ma中不满足简并碱基的点仍排除
+        # rm_non-ATCG_genomes.ma, sites that do not satisfy the simplex base are still excluded
         if len(uniq_nc) > 1 and tmp_all > f_base:
             cur = 0
             for key in uniq_nc.keys():
@@ -91,7 +78,7 @@ def pis_freq(align_filter, sites, seq, f_base):
     return freqs
 
 
-# rm_pis根据remove_fq设置了参数，remove_fq频率以上的简约信息位点只包含A,T,C,G序列
+# rm_pis is a parameter set according to remove_fq. The parsimonious information loci above the remove_fq frequency contain only A,T,C,G sequences
 def rm_N_list(alignments, rm_pis, filter_seq):
     sig_nc = {'a', 't', 'g', 'c', 'A', 'T', 'C', 'G'}
     for alignment in tqdm(alignments, desc='generating ePIS only with A,T,C,G'):
@@ -106,13 +93,13 @@ def rm_N_list(alignments, rm_pis, filter_seq):
             SeqIO.write(alignment, filter_seq, "fasta")
 
 
-# 还原多序列比对后的位置
+# Restore the position after multiple sequence alignment
 def get_ref_pos(ref_seq):
     fa_pos = []
     nt_pos = 0
     for nt in range(0, len(ref_seq)):
         if ref_seq[nt] != '-':
-            # 实际位置是下标加1
+            # The actual sequence position is the subscript +1
             nt_pos += 1
             fa_pos.append(nt_pos)
         else:
@@ -120,9 +107,8 @@ def get_ref_pos(ref_seq):
     return fa_pos
 
 
-# 根据简并信息位点输出新的fasta文件，出简并信息位点位点外的其他位点均为-
+# Output fasta files containing only parsimonious information loci
 def write_pis(alignment, all_pis, pos_fa):
-    #生成一个长度和alignment结果各条序列长度一致，且sites位点为1。其他位点为0的向量
     pos_boolean = np.zeros(len(alignment[0]))
     pos_boolean[all_pis] = 1
     pos_boolean = list(pos_boolean)
@@ -140,7 +126,6 @@ def write_pis(alignment, all_pis, pos_fa):
 
 
 if __name__ == '__main__':
-    # 参数设置
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--inpath', dest='inpath', help="input file path", required=True)
     parser.add_argument('-m', '--ma', dest='ma', help="input multiple alignment result file", required=True)
@@ -148,13 +133,11 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--remove', dest='remove', help="pis frequence cutoff of remove genomes, none if unknown, 0 if remove all non-A,T,C,G bases", required=True)
     parser.add_argument('-f', '--reference', dest='reference', help="id of reference sequence in the multiple alignment result file", required=True)
     args = parser.parse_args()
-    # 加载参数
-    # 设置输入路径
+
     if args.inpath[-1] != '/':
         inpath = args.inpath + "/"
     else:
         inpath = args.inpath
-    # 读取输入aligment文件
     try:
         align = AlignIO.read(inpath + args.ma, "fasta")
     except ValueError:
@@ -162,16 +145,13 @@ if __name__ == '__main__':
         print("Please check the input file format")
         sys.exit()
 
-    # 设置数据freq和pi_pos文件名
     freq_out = open(inpath + 'freq_all.txt', 'w')
     pos_out = open(inpath + 'pi_pos_all.fasta', 'w')
-
-    # 设置有效碱基数 (判断用户输入的必须是整数)
+)
     if args.base == 'none':
         base = int(len(align)*0.8)
     else:
         base = int(args.base)
-    # 设置需要排除ATCG情况的pis的频率阈值 (判断用户输入的必须是float)
     if args.remove == 'none':
         remove = 1/pow(10, len(str(len(align)))-2)
     else:
@@ -181,7 +161,6 @@ if __name__ == '__main__':
     ref_flag = 0
     ref_id = args.reference
     for gn in align:
-        # OEAV139851, AY390556.1, OEAV118620，OEAV121000
         if gn.id == ref_id:
             ref_seq = gn.seq
             ref_flag = 1
@@ -190,7 +169,6 @@ if __name__ == '__main__':
         sys.exit("No reference genome, please check the input file")
     ref_pos = get_ref_pos(ref_seq)
 
-    # 根据阈值去除pis中不是ATCG的序列
     all_sites = []
     rm_sites = []
     if os.path.exists(inpath + "rm_non-ATCG_genomes.ma") and os.path.getsize(inpath + "rm_non-ATCG_genomes.ma"):
@@ -205,10 +183,7 @@ if __name__ == '__main__':
         filter_file.close()
         alignments_filtered = AlignIO.read(inpath + "rm_non-ATCG_genomes.ma", "fasta")
 
-    # 加载去除非A,T,C,G后的序列后，根据过滤后的alignment文件包含基因组的个数设置对应的base值。
     base_filtered = int(len(alignments_filtered)*0.8)
-
-    # 输出频率文件，第一列为在reference中的位置，第二列为alignment结果位置，第三列为位点频率
     freq_out.write("reference")
     freq_out.write("\t")
     freq_out.write("alignment_pos")
@@ -218,7 +193,7 @@ if __name__ == '__main__':
     freq_out.write("depth")
     freq_out.write("\n")
 
-    # 这里需要注意直接生成的sites是从0开始，从freq_all文件得到的sites下标是从1开始
+    # Directly generated sites start at 0. The sites subscript from the freq_all file starts at 1
     write_sites = []
     all_freq = pis_freq(alignments_filtered, all_sites, ref_seq, base_filtered)
     for freq_cur in range(0, len(all_freq)):
